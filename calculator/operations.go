f3072cfcea027033da19242f5b9fc61d8ce0db82
MariamA_Helal <mariamamr28624@gmail.com>
2026-07-21 19:28:23 +0300

    feat: implement core math operations

--- calculator/operations.go
+++ calculator/operations.go
@@ -1,21 +1,31 @@
 package calculator
 
-func Add(a, b float64) float64 {
+import (
+	"errors"
+	"math"
+)
 
+func Add(a, b float64) float64 {
+	return a + b
 }
 
 func Sub(a, b float64) float64 {
-
+	return a - b
 }
 
 func Multi(a, b float64) float64 {
-
+	return a * b
 }
 
 func Div(a, b float64) (float64, int, error) {
-
+	if b == 0 {
+		return 0, 0, errors.New("Math Error: Can't divide by 0")
+	}
+	decAns := a / b
+	remainder := int(a) % int(b)
+	return decAns, remainder, nil
 }
 
 func Power(a, b float64) float64 {
-
+	return math.Pow(a, b)
 }
