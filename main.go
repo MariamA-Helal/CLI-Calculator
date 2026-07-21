4864994d534079430346c9e64ff6ef00a22dbe0f
MariamA_Helal <mariamamr28624@gmail.com>
2026-07-21 20:20:07 +0300

    test: test the math opertaions and ensure the error handeling

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/MariamA-Helal/CLI-Calculator/calculator"
+)
 
 func main() {
-	fmt.Println("CLI Calculator Started...")
+	fmt.Println("==== CLI Calculator Started... ====")
+
+	sum := calculator.Add(5.73, 4.538)
+	fmt.Printf("Test add: 5.73 + 4.538 = %.2f \n", sum)
+
+	decAns, remainder, err := calculator.Div(10, 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Test div: 10 / 3 = %.2f \n(Remainder: %d) \n", decAns, remainder)
+	}
+
 }
