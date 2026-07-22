d40f06e4b8023f44be172f2a55a6bc4cb95aaba0
MariamA_Helal <mariamamr28624@gmail.com>
2026-07-22 03:42:07 +0300

    feat: recive user input via terminal loop

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/MariamA-Helal/CLI-Calculator/calculator"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("==== CLI Calculator Started... ====")
+	fmt.Println("Type 'exit' or 'quit' to close the program.")
+	fmt.Println("--------------------------------------------")
 
-	sum := calculator.Add(5.73, 4.538)
-	fmt.Printf("Test add: 5.73 + 4.538 = %.2f \n", sum)
+	reader := bufio.NewReader(os.Stdin)
 
-	decAns, remainder, err := calculator.Div(10, 0)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("Test div: 10 / 3 = %.2f \n(Remainder: %d) \n", decAns, remainder)
+	for {
+		fmt.Print("Calc> ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+
+		if input == "" {
+			continue
+		}
+
+		if strings.ToLower(input) == "exit" || strings.ToLower(input) == "quit" {
+			fmt.Println("Exiting calculator ... ")
+			break
+		}
+
+		fmt.Printf(" you entered: %s\n ", input)
+
 	}
 
+	// sum := calculator.Add(5.73, 4.538)
+	// fmt.Printf("Test add: 5.73 + 4.538 = %.2f \n", sum)
+
+	// decAns, remainder, err := calculator.Div(10, 0)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Printf("Test div: 10 / 3 = %.2f \n(Remainder: %d) \n", decAns, remainder)
+	// }
+
 }
+
+//
